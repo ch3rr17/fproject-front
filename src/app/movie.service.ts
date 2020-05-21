18a61ec9b163a32cf653fdf107bef02873759ca5
Cherr Batac <ch3rr17>
2020-05-20 17:25:26 -0700

    adds observable option

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MovieService {
+  movies: Observable<any[]>;
+  movieUrl = 'https://api.themoviedb.org/3/search/movie?query=${title}&api_key=8caa54fd5e1463137672ebbd6c42fee1';
 
   constructor(public http: HttpClient) { }
 
-  getMovies(title) {
-    const url =`https://api.themoviedb.org/3/search/movie?query=${title}&api_key=8caa54fd5e1463137672ebbd6c42fee1`;
+  getMovies(): Observable<Movies []> {
+    // const url =`https://api.themoviedb.org/3/search/movie?query=${title}&api_key=8caa54fd5e1463137672ebbd6c42fee1`;
     console.log('URL', url);
     return this.http.get(url);
   }
